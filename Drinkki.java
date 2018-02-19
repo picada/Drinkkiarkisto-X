cec0651fbcbc50bef8f3496b9abfecae40eac1c7
Chenhui ZHao <32270417+lchz@users.noreply.github.com>
2018-02-19 19:47:42 +0200

    Add files via upload

---
+++ Drinkki.java
@@ -0,0 +1,27 @@
+package tikape.drinkkiarkisto;
+
+public class Drinkki {
+    String nimi;
+    Integer id;
+    
+    Drinkki(Integer id, String nimi) {
+        this.id = id;
+        this.nimi = nimi;
+    }
+    
+    public Integer getId() {
+        return this.id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public String getNimi() {
+        return this.nimi;
+    }
+    
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+}
